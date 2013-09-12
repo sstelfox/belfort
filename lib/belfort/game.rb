ab0a89382f69cb942919629bd3b891f5e1cd9f86
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-12 18:42:28 -0400

    More tests

--- lib/belfort/game.rb
+++ lib/belfort/game.rb
@@ -4,6 +4,9 @@ module Belfort
     attr_reader :players
 
     def initialize(player_count)
+      unless player_count.between?(2, 5)
+        fail(ArgumentError, "Player count has to be between 2 and 5") 
+      end
       @players = Array.new(player_count)
     end
   end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,12 +2,38 @@
 require 'spec_helper'
 
 describe Belfort::Game do
-  subject { described_class.new(player_count) }
+  context "#initialize" do
+    it "should raise an error with too few players" do
+      expect { described_class.new(1) }.to raise_error(ArgumentError)
+    end
+
+    it "should raise an error with too many players" do
+      expect { described_class.new(6) }.to raise_error(ArgumentError)
+    end
+  end
 
   context "#players" do
-    let(:player_count) { 2 }
+    subject { described_class.new(player_count) }
 
-    it { should have(2).players }
+    context "two players" do
+      let(:player_count) { 2 }
+      it { expect(subject.players.size).to eql(2) }
+    end
+
+    context "two players" do
+      let(:player_count) { 3 }
+      it { expect(subject.players.size).to eql(3) }
+    end
+
+    context "two players" do
+      let(:player_count) { 4 }
+      it { expect(subject.players.size).to eql(4) }
+    end
+
+    context "two players" do
+      let(:player_count) { 5 }
+      it { expect(subject.players.size).to eql(5) }
+    end
   end
 end
 
