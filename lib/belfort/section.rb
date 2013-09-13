ed285d6f034bf4731ae861bc70559ef6e406a7d6
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-13 00:51:56 -0400

    A little bit more progress

--- lib/belfort/section.rb
+++ lib/belfort/section.rb
@@ -5,8 +5,19 @@ module Belfort
   class Section
 
     # Setup an empty section of the board.
+    #
+    # @param [Belfort::Board] board The board this section belongs too.
     def initialize(board)
-      @places = SECTION_AREAS.zip([nil])
+      @board = board
+      @places = Hash[SECTION_AREAS.zip([nil])]
+    end
+
+    # Check to see whether a particular location is available or not.
+    #
+    # @param [Symbol] location A named location to be checked.
+    def available?(location)
+      fail(ArgumentError, "Invalid location") unless SECTION_AREAS.include?(location)
+      !(@places.fetch(location))
     end
   end
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -2,5 +2,11 @@
 require 'spec_helper'
 
 describe Belfort::Section do
+  let(:board) { double("Belfort::Board") }
+  subject { described_class.new(board) }
+
+  it "should be able to report on available locations" do
+    subject.available?(:bank).should be_true
+  end
 end
 
