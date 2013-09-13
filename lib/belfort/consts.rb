d9cec3e6c27f4e03748b9e4412be4e76ee54b92a
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-13 00:22:48 -0400

    Added in list of section areas

--- lib/belfort/consts.rb
+++ lib/belfort/consts.rb
@@ -2,4 +2,10 @@
 module Belfort
   # An ordered list of the rounds that occur within the game.
   ROUNDS = [:calendar, :placement, :collection, :actions, :scoring]
+
+  # The list of the various places that exist within a section for a player to
+  # place pieces (with the exception of guilds)
+  SECTION_AREAS = [ :bank, :blacksmith, :gardens, :gatehouse_left,
+    :gatehouse_right, :inn, :keep_left, :keep_right, :library, :market, :pub,
+    :tower ]
 end
