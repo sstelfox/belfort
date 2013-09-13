ae1fb9fa4f46833fde52945fdc8883ab7c8e6252
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-12 20:48:07 -0400

    Refactored game class

--- lib/belfort/game.rb
+++ lib/belfort/game.rb
@@ -3,14 +3,22 @@ module Belfort
   class Game
     attr_reader :phase, :players, :round
 
-    def initialize(player_count)
-      unless player_count.between?(2, 5)
+    def initialize(player_count, options = {})
+      @player_count = options.fetch(:count, player_count)
+
+      unless @player_count.between?(2, 5)
         fail(ArgumentError, "Player count has to be between 2 and 5") 
       end
 
       @phase = ROUNDS.first
-      @players = player_count.times.map { |n| Player.new("Player #{n + 1}") }
+      @players = options.fetch(:players, default_players)
       @round = 0
+    end
+
+    private
+
+    def default_players
+      @player_count.times.map { |n| Player.new("Player #{n + 1}") }
     end
   end
 end
