225c2ebddba6c1d74edb2b98a71d31e6050ad60a
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-12 20:58:20 -0400

    Spec refactor

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,11 +2,7 @@
 require 'spec_helper'
 
 describe Belfort::Game do
-  subject { described_class.new(count: player_count) }
-
   context "#initialize" do
-    let(:player_count) { 2 }
-
     it "should raise an error with too few players" do
       expect { described_class.new(count: 1) }.to raise_error(ArgumentError)
     end
@@ -24,75 +20,79 @@ describe Belfort::Game do
     end
   end
 
-  context "with two players" do
-    let(:player_count) { 2 }
+  context "with players:" do
+    subject { described_class.new(count: player_count) }
 
-    it { should have(2).players }
+    context "two" do
+      let(:player_count) { 2 }
 
-    it "should generate Belfort::Players" do
-      subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
+      it { should have(2).players }
+
+      it "should generate Belfort::Players" do
+        subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
+      end
+
+      it "should have 'Player 1' for the first player's name" do
+        subject.players.first.name.should eql("Player 1")
+      end
+
+      it "should have 'Player 2' for the last player's name" do
+        subject.players.last.name.should eql("Player 2")
+      end
     end
 
-    it "should have 'Player 1' for the first player's name" do
-      subject.players.first.name.should eql("Player 1")
+    context "three" do
+      let(:player_count) { 3 }
+
+      it { should have(3).players }
+
+      it "should generate Belfort::Players" do
+        subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
+      end
+
+      it "should have 'Player 1' for the first player's name" do
+        subject.players.first.name.should eql("Player 1")
+      end
+
+      it "should have 'Player 3' for the last player's name" do
+        subject.players.last.name.should eql("Player 3")
+      end
     end
 
-    it "should have 'Player 2' for the last player's name" do
-      subject.players.last.name.should eql("Player 2")
-    end
-  end
+    context "four" do
+      let(:player_count) { 4 }
 
-  context "with three players" do
-    let(:player_count) { 3 }
+      it { should have(4).players }
 
-    it { should have(3).players }
+      it "should generate Belfort::Players" do
+        subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
+      end
 
-    it "should generate Belfort::Players" do
-      subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
-    end
+      it "should have 'Player 1' for the first player's name" do
+        subject.players.first.name.should eql("Player 1")
+      end
 
-    it "should have 'Player 1' for the first player's name" do
-      subject.players.first.name.should eql("Player 1")
-    end
-
-    it "should have 'Player 3' for the last player's name" do
-      subject.players.last.name.should eql("Player 3")
-    end
-  end
-
-  context "with four players" do
-    let(:player_count) { 4 }
-
-    it { should have(4).players }
-
-    it "should generate Belfort::Players" do
-      subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
+      it "should have 'Player 4' for the last player's name" do
+        subject.players.last.name.should eql("Player 4")
+      end
     end
 
-    it "should have 'Player 1' for the first player's name" do
-      subject.players.first.name.should eql("Player 1")
-    end
+    context "five" do
+      let(:player_count) { 5 }
 
-    it "should have 'Player 4' for the last player's name" do
-      subject.players.last.name.should eql("Player 4")
-    end
-  end
+      it { should have(5).players }
 
-  context "with five players" do
-    let(:player_count) { 5 }
+      it "should generate Belfort::Players" do
+        subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
+      end
 
-    it { should have(5).players }
+      it "should have 'Player 1' for the first player's name" do
+        subject.players.first.name.should eql("Player 1")
+      end
 
-    it "should generate Belfort::Players" do
-      subject.players.each { |p| p.should be_instance_of(Belfort::Player) }
-    end
-
-    it "should have 'Player 1' for the first player's name" do
-      subject.players.first.name.should eql("Player 1")
-    end
-
-    it "should have 'Player 5' for the last player's name" do
-      subject.players.last.name.should eql("Player 5")
+      it "should have 'Player 5' for the last player's name" do
+        subject.players.last.name.should eql("Player 5")
+      end
     end
   end
 end
