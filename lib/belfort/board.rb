eb6bdbdcdd7a26806988158a99e8edf6e5aa1931
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-13 09:19:02 -0400

    Minor refactor

--- lib/belfort/board.rb
+++ lib/belfort/board.rb
@@ -6,7 +6,16 @@ module Belfort
 
     # Setup the board with a bunch of sections.
     def initialize
-      @sections = 5.times.map { Belfort::Section.new(self) }
+      @sections = default_sections
+    end
+    
+    private
+
+    # Handle the creation of the various board sections.
+    #
+    # @return [Array<Belfort::Section>] the built up sections.
+    def default_sections
+      5.times.map { Belfort::Section.new(self) }
     end
   end
 end
