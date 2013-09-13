c57f771f93bea08623f4046f55ded3e7353a937a
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-12 20:23:30 -0400

    Name adjustment

--- lib/belfort/player.rb
+++ lib/belfort/player.rb
@@ -1,10 +1,11 @@
 
 module Belfort
   class Player
-    attr_reader :wood, :stone, :metal, :gold, :name
+    attr_reader :wood, :stone, :metal, :gold
+    attr_accessor :name
 
     def initialize(name)
-      @name = name
+      self.name = name
       @wood = 1
       @stone = 1
       @metal = 1
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,15 +2,20 @@
 require 'spec_helper'
 
 describe Belfort::Player do
-  context "just initialized" do
-    let(:name) { "Jinx" }
-    subject { described_class.new(name) }
+  let(:name) { "Jinx" }
+  subject { described_class.new(name) }
 
+  context "just initialized" do
     its(:wood)  { should eql(1) }
     its(:stone) { should eql(1) }
     its(:metal) { should eql(1) }
     its(:gold)  { should eql(5) }
     its(:name)  { should eql("Jinx") }
+  end
+
+  it "should allow for updating names" do
+    new_name = "John"
+    expect { subject.name = new_name}.to change { subject.name }.from(name).to(new_name)
   end
 end
 
