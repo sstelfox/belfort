54267e7533862a5fcc76d01537ad7e059814b8d0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-14 00:16:10 -0400

    Added code coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 
+require 'cover_me'
 require 'belfort'
+
+CoverMe.config do |c|
+  c.at_exit = Proc.new {
+    File.join(CoverMe.config.html_formatter.output_path, 'index.html')
+  }
+end
+CoverMe.complete!
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
